e31e6bd071656ed376dbe36f059a0c19a1750284
Xavier Denis <xldenis@gmail.com>
2017-01-10 22:02:29 -0600

    Remove isbn

--- src/GBooks.hs
+++ src/GBooks.hs
@@ -13,7 +13,12 @@ import Data.Aeson.Lens
 instance MonadHttp IO where
   handleHttpException = throwIO
 
-data Book = Book { title :: Text, author :: Text, description :: Text, isbn :: Text } deriving (Show, Eq)
+data Book = Book
+  { title :: Text
+  , author :: Text
+  , description :: Text
+  -- , isbn :: Text
+  } deriving (Show, Eq)
 
 searchByTitle :: MonadHttp m => Text -> m (Maybe Book)
 searchByTitle title = do
@@ -23,9 +28,16 @@ searchByTitle title = do
     "q" =: title
   return $ (responseBody v :: Value) ^? (key "items") . nth 0 . key "volumeInfo" >>= toBook
 
+doSearch title = do
+  let url = https "www.googleapis.com" /: "books" /: "v1" /: "volumes"
+
+  req GET url NoReqBody jsonResponse $
+    "q" =: title
+
 toBook :: Value -> Maybe Book
 toBook value = Book
   <$> value ^? key "title" . _String
   <*> value ^? key "authors" . nth 0 . _String
   <*> value ^? key "description" . _String
-  <*> value ^? key "industryIdentifiers" . _Array . to (V.filter $ \o -> o ^?! key "type" == "ISBN_13") . ix 0 . key "identifier" . _String
+  -- <*> value ^? key "industryIdentifiers" . _Array . _Isbn13 . ix 0 . key "identifier" . _String
+  -- where _Isbn13 = to (V.filter $ \o -> o ^?! key "type" == "ISBN_13")
